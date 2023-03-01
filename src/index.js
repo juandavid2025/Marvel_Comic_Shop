7abd89b3a915ca41d7d7ea219e4eedcb34f5e09d
David Aguirre <david.aguirreq@perficient.com>
2023-03-01 09:08:31 -0500

    Remove unnecessary code line comments in index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
-// import React from "react";
-// import { render } from "react-dom";
-// import App from "./App";
-// import reportWebVitals from "./reportWebVitals";
-
-// import store from "./store/index";
-// import { Provider } from "react-redux";
-// import { BrowserRouter } from "react-router-dom";
-// import "./index.css";
-
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -26,12 +16,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-// render(
-//   <Provider store={store}>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </Provider>,
-//   document.getElementById("root")
-// );
